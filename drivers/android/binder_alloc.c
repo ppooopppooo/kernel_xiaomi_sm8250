573f58179037a6cb4bcd5383cc4e634b375fa392
Will McVicker <willmcvicker@google.com>
2020-04-27 11:38:33 -0700

    Revert "dma-buf: Refactor dma-buf name"
    This reverts commit 81ea26379b491793f2de8ee3ecac3531645d1dd5. The change
    breaks DMA_BUF_SET_NAME ioctl by improperly duplicating the dma_buf
    struct field 'const char *name'.
    
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Bug: 153823050
    Test: boot bramble, verify list of probed devices
    Change-Id: I716513d1e19449616eaa368d2c7de48ce479df3e

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -279,11 +279,7 @@ static int binder_update_page_range(struct binder_alloc *alloc, int allocate,
 		/* vm_insert_page does not seem to increment the refcount */
 	}
 	if (mm) {
-<<<<<<< HEAD
-		up_read(&mm->mmap_sem);
-=======
 		mmap_write_unlock(mm);
->>>>>>> e680cd14cc5e (BACKPORT: mmap locking API: use coccinelle to convert mmap_sem rwsem call sites)
 		mmput_async(mm);
 	}
 	return 0;
@@ -316,11 +312,7 @@ err_page_ptr_cleared:
 	}
 err_no_vma:
 	if (mm) {
-<<<<<<< HEAD
-		up_read(&mm->mmap_sem);
-=======
 		mmap_write_unlock(mm);
->>>>>>> e680cd14cc5e (BACKPORT: mmap locking API: use coccinelle to convert mmap_sem rwsem call sites)
 		mmput_async(mm);
 	}
 	return vma ? -ENOMEM : -ESRCH;
