12a084454a4ed3acf762258f1d6e2e5556380193
Nathan Chancellor <nathan@kernel.org>
2022-03-30 16:45:27 -0700

    Revert "msm: eva: put kref after processing deprecated message"
    This reverts commit 826e09b64c8c1cbbfd8452a41104e9531c674e5a.

--- drivers/media/platform/msm/cvp/hfi_response_handler.c
+++ drivers/media/platform/msm/cvp/hfi_response_handler.c
@@ -504,7 +504,6 @@ static int hfi_process_session_cvp_msg(u32 device_id,
 			|| pkt->packet_type == HFI_MSG_SESSION_CVP_FD) {
 			u64 ktid;
 			u32 kdata1, kdata2;
-			int rc;
 
 			kdata1 = pkt->client_data.kdata1;
 			kdata2 = pkt->client_data.kdata2;
@@ -516,10 +515,8 @@ static int hfi_process_session_cvp_msg(u32 device_id,
 
 			msm_cvp_unmap_buf_cpu(inst, ktid);
 
-			rc = _deprecated_hfi_msg_process(device_id, pkt, info,
-							 inst);
-			cvp_put_inst(inst);
-			return rc;
+			return _deprecated_hfi_msg_process(device_id,
+				pkt, info, inst);
 		}
 		dprintk(CVP_ERR, "Invalid deprecate_bitmask %#x\n",
 					inst->deprecate_bitmask);
