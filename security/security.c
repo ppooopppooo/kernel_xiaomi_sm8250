3da2f6c4269ef02d917af1b86ed166ef06a4a89b
ppooopppooo <ppooopppooo@qq.com>
2025-11-10 07:57:16 +0800

    Revert "new helper: security_sb_eat_lsm_opts()"
    This reverts commit c10e90011597f07eb7c3080a4723a0f1d300822f.

--- security/security.c
+++ security/security.c
@@ -380,20 +380,11 @@ void security_sb_free(struct super_block *sb)
 	call_void_hook(sb_free_security, sb);
 }
 
-int security_sb_eat_lsm_opts(char *options, struct security_mnt_opts *opts)
+int security_sb_copy_data(char *orig, char *copy)
 {
-	char *s = (char *)get_zeroed_page(GFP_KERNEL);
-	int err;
-
-	if (!s)
-		return -ENOMEM;
-	err = call_int_hook(sb_copy_data, 0, options, s);
-	if (!err)
-		err = call_int_hook(sb_parse_opts_str, 0, s, opts);
-	free_page((unsigned long)s);
-	return err;
+	return call_int_hook(sb_copy_data, 0, orig, copy);
 }
-EXPORT_SYMBOL(security_sb_eat_lsm_opts);
+EXPORT_SYMBOL(security_sb_copy_data);
 
 int security_sb_remount(struct super_block *sb, void *data)
 {
